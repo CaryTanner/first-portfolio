0bd6c9436fc3cfc0a4fa354606615cc94219e1de
CaryTanner <cary.landon@gmail.com>
2020-09-29 17:39:41 +0200

    update nav burger

--- dist/scripts.js
+++ dist/scripts.js
@@ -5,3 +5,8 @@ hamburgerBtn.addEventListener('click', () => {
     hamburgerBtn.parentNode.classList.toggle('hide')
 })
     
+let buttonAnimation = document.querySelector('#nav-burger')
+
+buttonAnimation.addEventListener('click', () => {
+    buttonAnimation.classList.toggle('open')
+})
