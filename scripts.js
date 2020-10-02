862eabf722589a607d0feb8fe4f5ed06306f8f93
CaryTanner <cary.landon@gmail.com>
2020-10-02 16:56:28 +0200

    Updates

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
-let hamburgerBtn = document.querySelector('.menu-button')
+//nav hamburger menu
 
+let hamburgerBtn = document.querySelector(".menu-button");
+let nav = document.querySelector("nav");
 
-hamburgerBtn.addEventListener('click', () => {
-    hamburgerBtn.parentNode.classList.toggle('hide')
-})
-    
+hamburgerBtn.addEventListener("click", () => {
+  nav.classList.toggle("hide");
+});
+
+let buttonAnimation = document.querySelector("#nav-burger");
+
+buttonAnimation.addEventListener("click", () => {
+  buttonAnimation.classList.toggle("open");
+});
